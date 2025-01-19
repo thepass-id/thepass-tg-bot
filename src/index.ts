ff496ca47957b8156b6a2422efdb80a3603d9b49
byxaman <52375046+byxaman@users.noreply.github.com>
2025-01-19 17:33:53 +0300

    Feature Updates and Bug Fixes (#4)
    * Initial commit
    
    * change tg bot environment
    
    * change tg bot environment
    
    * update server
    
    * update bodyParser limit
    
    * fixed check result transaction ulr

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import { Telegraf, Markup } from 'telegraf';
+import dotenv from 'dotenv';
+import QRCode from 'qrcode';
+
+import './server';
+
+dotenv.config();
+
+// ENV
+const botToken = process.env.BOT_TOKEN!;
+const thePassTgBotUrl = process.env.THE_PASS_TG_BOT_URL!;
+
+export const telegramBot = new Telegraf(botToken, {
+  telegram: { testEnv: true },
+});
+
+telegramBot.telegram.setMyCommands([
+  {
+    command: 'start',
+    description: 'Test command',
+  },
+  {
+    command: 'addproof',
+    description: 'Create a ne QR code to validate proof',
+  },
+]);
+
+telegramBot.start((ctx) => {
+  return ctx.reply("Let's get started 🚀", {
+    reply_markup: Markup.inlineKeyboard([
+      [
+        Markup.button.url('Open the Pass app', 't.me/thePassbot/thePassApp'),
+        Markup.button.callback(
+          'Generate QR code to validate proof',
+          'submit_proof'
+        ),
+      ],
+    ]).reply_markup,
+  });
+});
+
+telegramBot.command('addproof', (ctx) => {
+  return ctx.reply('You can click the button below', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Submit the proof!',
+            callback_data: 'submit_proof',
+          },
+        ],
+      ],
+    },
+  });
+});
+
+telegramBot.action('submit_proof', async (ctx) => {
+  const userId = ctx.from.id;
+
+  const chatId = ctx.chat?.id;
+  if (!chatId) throw new Error('Chat id not found');
+
+  const qrBuffer = await QRCode.toBuffer(
+    `${thePassTgBotUrl}/submitproof/${chatId}`,
+    { width: 200 }
+  );
+
+  return ctx.replyWithPhoto({ source: qrBuffer });
+});
+
+telegramBot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => telegramBot.stop('SIGINT'));
+process.once('SIGTERM', () => telegramBot.stop('SIGTERM'));
---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import { sendTransactionResultToTelegram, validateProof } from './utils';
+
+dotenv.config();
+
+// ENV
+const port = process.env.APP_PORT!;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '10mb' }));
+
+app.post('/submitproof/:telegramChatId', async (req, res) => {
+  try {
+    const { telegramChatId } = req.params;
+    const { proof } = req.body;
+
+    const txHash = await validateProof(telegramChatId, proof);
+    await sendTransactionResultToTelegram(telegramChatId, txHash);
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.warn(error);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`);
+});
